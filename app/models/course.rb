4d997df2b964694f9e8ff859cc66bfb85ad2c99f
“iseitz” <irenika@gmail.com>
2023-02-14 08:00:29 +0000

    Worked through Capybara test of reachfield text area

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,9 +2,15 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { :minimum => 5 }
   has_rich_text :description
+  # after_validation :validate_course_decription
   
   def to_s
     title 
   end
   
+  # def validate_course_decription
+  #   if self.description.nil?
+  #   self.errors[:description] << ["Course description can not be blanc"] 
+  #   end
+  # end
 end
--- features/step_definitions/course_steps.rb
+++ features/step_definitions/course_steps.rb
@@ -19,6 +19,19 @@ When('I create a course record with title and description') do
   visit "/"
   expect(page).to have_content("If you are an authorized teacher add your course here")
   click_on "add_new_course"
+  expect(page).to have_content("New course")
+  fill_in "course_title", with: "My new course for coding 1"
+  expect(page).to have_css '.trix-content'
+  # expect(page).to have_css 'course_description_trix_input_course'
+  find(:xpath, "//*[@id='course_description_trix_input_course']", visible: false).set("description of my test course")
+  # screenshot_and_save_page
+  sleep 3
+  click_on "course_submit"
+  expect(page).to have_content("Course was successfully created.")
+  expect(page).to have_content("My new course for coding 1")
+  expect(page).to have_content("description of my test course")
+  expect(page).to have_content("Edit")
+  expect(page).to have_content("Back")
 end
 
 Then('I should see a record of the course and a confirmation message') do
