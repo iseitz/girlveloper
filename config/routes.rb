bef30ffc906939b611be1ca8b5b42e49b95e913b
“iseitz” <irenika@gmail.com>
2023-02-22 09:45:21 +0000

    Added devise trackable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :courses
   devise_for :users
+  resources :courses
+  resources :users, only: [:index]
   root "sites#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
