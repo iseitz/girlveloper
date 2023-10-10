f758e4b1b1879674d044860e61fe02d229e328fa
“iseitz” <irenika@gmail.com>
2023-10-10 11:36:34 +0000

    Devise mailer fixed error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,7 +29,7 @@ Devise.setup do |config|
   
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
-  config.mailer = 'Application::Mailer'
+  config.mailer = 'ApplicationMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
